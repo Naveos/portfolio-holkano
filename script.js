ba5bbb34666bc4d755e6273acfb5e32ea9aed2cb
Naveos <108453538+Naveos@users.noreply.github.com>
2023-03-27 11:00:45 +0200

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('discord').style.display = "none";
+document.getElementById('instagram').style.display = "none";
+document.getElementById('discordbtn').onclick = function () {
+    document.getElementById('discord').style.display = "inline";
+}
+document.getElementById('instabtn').onclick = function () {
+    document.getElementById('instagram').style.display = "inline";
+}
